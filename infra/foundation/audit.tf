bc4642d201634c50155bf0c4c12cfd36d5a7b7b2
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2026-08-20 11:18:33 +0000

    Match the reserved default log sink

--- infra/foundation/audit.tf
+++ infra/foundation/audit.tf
@@ -37,13 +37,20 @@ resource "google_logging_project_bucket_config" "controlgraph" {
 # _Default is provider-created. The Google provider acquires and updates this
 # reserved sink instead of attempting to create or delete it.
 resource "google_logging_project_sink" "default" {
-  project                = var.project_id
-  name                   = "_Default"
-  description            = "Route ControlGraph logs to the dedicated regional bucket."
-  destination            = "logging.googleapis.com/projects/${var.project_id}/locations/${var.region}/buckets/${google_logging_project_bucket_config.controlgraph.bucket_id}"
-  filter                 = ""
+  project     = var.project_id
+  name        = "_Default"
+  description = "Route ControlGraph logs to the dedicated regional bucket."
+  destination = "logging.googleapis.com/projects/${var.project_id}/locations/${var.region}/buckets/${google_logging_project_bucket_config.controlgraph.bucket_id}"
+  filter = join(" AND ", [
+    "NOT LOG_ID(\"cloudaudit.googleapis.com/activity\")",
+    "NOT LOG_ID(\"externalaudit.googleapis.com/activity\")",
+    "NOT LOG_ID(\"cloudaudit.googleapis.com/system_event\")",
+    "NOT LOG_ID(\"externalaudit.googleapis.com/system_event\")",
+    "NOT LOG_ID(\"cloudaudit.googleapis.com/access_transparency\")",
+    "NOT LOG_ID(\"externalaudit.googleapis.com/access_transparency\")",
+  ])
   disabled               = false
-  unique_writer_identity = false
+  unique_writer_identity = true
 
   lifecycle {
     prevent_destroy = true
